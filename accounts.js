c8d66559a2ab533bd71c487a21853697e872ab3b
KOF <hmooduu6@gmail.com>
2026-01-12 17:42:27 +0300

    Implement user account management functions

--- accounts.js
+++ accounts.js
@@ -1,2 +1,32 @@
 // accounts.js
-console.log('هذا هو ملف accounts.js - إدارة حسابات المستخدمين');
+const accounts = new Map(); 
+// username => { password, discordId }
+
+function createAccount(username, password, discordId) {
+  accounts.set(username, {
+    password,
+    discordId
+  });
+}
+
+function login(username, password) {
+  const acc = accounts.get(username);
+  if (!acc) return false;
+  if (acc.password !== password) return false;
+  return acc;
+}
+
+function getByUsername(username) {
+  return accounts.get(username);
+}
+
+function getAllAccounts() {
+  return accounts;
+}
+
+module.exports = {
+  createAccount,
+  login,
+  getByUsername,
+  getAllAccounts
+};
