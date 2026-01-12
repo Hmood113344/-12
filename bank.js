76758a0d836f871316271af183ccf64bba15160b
KOF <hmooduu6@gmail.com>
2026-01-12 17:42:52 +0300

    Implement basic banking functions in bank.js

--- bank.js
+++ bank.js
@@ -1,2 +1,25 @@
 // bank.js
-console.log('هذا هو ملف bank.js - نظام بنك RP');
+const bank = new Map();
+// discordId => balance
+
+function getBalance(id) {
+  if (!bank.has(id)) bank.set(id, 50000);
+  return bank.get(id);
+}
+
+function addBalance(id, amount) {
+  bank.set(id, getBalance(id) + amount);
+}
+
+function deductBalance(id, amount) {
+  const bal = getBalance(id);
+  if (bal < amount) return false;
+  bank.set(id, bal - amount);
+  return true;
+}
+
+module.exports = {
+  getBalance,
+  addBalance,
+  deductBalance
+};
